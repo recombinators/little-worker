29d35b50c8d3f62987edc4191594c92091b98487
Mark Saiget <saiget@gmail.com>
2015-03-26 11:52:13 -0700

    Clean up string formats... still needs more cleaning. Configure method to allow user to request scene and bands. Delete files when done.

--- app/app/views.py
+++ app/app/views.py
@@ -9,8 +9,9 @@ from homura import download
 from downloader import Downloader
 from image import Process
 from pyramid.httpexceptions import HTTPFound
+from shutil import rmtree
 
-dir = os.getcwd()
+direc = os.getcwd()
 
 AWS_ACCESS_KEY_ID = os.environ['AWS_ACCESS_KEY_ID']
 AWS_SECRET_ACCESS_KEY = os.environ['AWS_SECRET_ACCESS_KEY']
@@ -18,7 +19,9 @@ AWS_SECRET_ACCESS_KEY = os.environ['AWS_SECRET_ACCESS_KEY']
 
 @view_config(route_name='home', renderer='json')
 def my_view(request):
-    dir = os.getcwd() + '/scenes'
+    # Check if image already exists.
+
+    direc = os.getcwd() + '/scenes'
     scene = request.matchdict['id']
     root = 'http://landsat-pds.s3.amazonaws.com/L8/'
     path = scene[3:6]
@@ -26,59 +29,83 @@ def my_view(request):
     b1 = request.matchdict['b1']
     b2 = request.matchdict['b2']
     b3 = request.matchdict['b3']
+    direc_scene = '{direc}/{scene}'.format(direc=direc, scene=scene)
+
+    direc_scene_scene = '{direc}/{scene}/{scene}'.format(direc=direc, scene=scene)
 
     # Builds a string pointing towards the AWS Landsat datasets
     # IE: http://landsat-pds.s3.amazonaws.com/L8/139/045/LC81390452014295LGN00/LC81390452014295LGN00_B3.TIF.ovr
-    o1 = root + path + '/' + row + '/' + scene + '/' + scene + '_B' + b1 + '.TIF.ovr'
-    o2 = root + path + '/' + row + '/' + scene + '/' + scene + '_B' + b2 + '.TIF.ovr'
-    o3 = root + path + '/' + row + '/' + scene + '/' + scene + '_B' + b3 + '.TIF.ovr'
+    o1 = '{root}{path}/{row}/{scene}/{scene}_B{band}.TIF.ovr'.format(
+                    root=root, path=path, row=row, scene=scene, band=b1)
+    o2 = '{root}{path}/{row}/{scene}/{scene}_B{band}.TIF.ovr'.format(
+                    root=root, path=path, row=row, scene=scene, band=b2)
+    o3 = '{root}{path}/{row}/{scene}/{scene}_B{band}.TIF.ovr'.format(
+                    root=root, path=path, row=row, scene=scene, band=b3)
+
+    # o1 = root + path + '/' + row + '/' + scene + '/' + scene + '_B' + b1 + '.TIF.ovr'
+    # o2 = root + path + '/' + row + '/' + scene + '/' + scene + '_B' + b2 + '.TIF.ovr'
+    # o3 = root + path + '/' + row + '/' + scene + '/' + scene + '_B' + b3 + '.TIF.ovr'
 
     # Create a subdirectory
-    if not os.path.exists(dir):
-        os.makedirs(dir)
+    if not os.path.exists(direc):
+        os.makedirs(direc)
 
-    # Download a sacrificial band using Landsat-util
-    dl = Downloader(verbose=True, download_dir=dir)
-    dl.download(['LC81390452014295LGN00'], ['1'])
+    # Download a sacrificial band(1) using Landsat-util
+    dl = Downloader(verbose=True, download_dir=direc)
+    dl.download([str(scene)], ['1'])
 
     # Strip the geospatial metadata from a legitimate Landsat band.
     # Create a world.tfw to reapply to previews.
-    subprocess.call(['listgeo', '-tfw', dir + '/' + scene + '/' + scene + '_B1.TIF'])
-    os.rename(dir + '/' + scene + '/' + scene + '_B1.tfw', dir + '/' + 'world.tfw')
+    subprocess.call(['listgeo', '-tfw', direc_scene_scene + '_B1.TIF'])
+    os.rename(direc_scene_scene + '_B1.tfw', direc + '/' + 'world.tfw')
 
     # Download previews from AWS
-    download(url=o1, path=dir)
-    download(url=o2, path=dir)
-    download(url=o3, path=dir)
+    download(url=o1, path=direc)
+    download(url=o2, path=direc)
+    download(url=o3, path=direc)
+    print 'done downloding previews from aws'
 
+    direc_world = '{}/world.tfw'.format(direc)
+    # direc_scene_band = '{direc}/{scene}_{band}.TIF.ovr'.format(direc=direc, scene=scene, band=b1)
     # Apply the stripped world file to the band previews.
-    subprocess.call(['geotifcp', '-e', dir + '/world.tfw', dir + '/' + scene + '_B2.TIF.ovr', dir + '/B2-geo.TIF'])
-    subprocess.call(['geotifcp', '-e', dir + '/world.tfw', dir + '/' + scene + '_B3.TIF.ovr', dir + '/B3-geo.TIF'])
-    subprocess.call(['geotifcp', '-e', dir + '/world.tfw', dir + '/' + scene + '_B5.TIF.ovr', dir + '/B5-geo.TIF'])
+    subprocess.call(['geotifcp', '-e', direc_world, '{direc}/{scene}_B{band}.TIF.ovr'.format(direc=direc, scene=scene, band=b1), direc + '/B' + b1 + '-geo.TIF'])
+    subprocess.call(['geotifcp', '-e', direc_world, '{direc}/{scene}_B{band}.TIF.ovr'.format(direc=direc, scene=scene, band=b2), direc + '/B' + b2 + '-geo.TIF'])
+    subprocess.call(['geotifcp', '-e', direc_world, '{direc}/{scene}_B{band}.TIF.ovr'.format(direc=direc, scene=scene, band=b3), direc + '/B' + b3 + '-geo.TIF'])
+    print 'done applying world file to previews'
 
     # Resize each band
-    # subprocess.call(['mkdir', dir + '/ready'])
-    subprocess.call(['gdal_translate', '-outsize', '15%', '15%', dir + '/B2-geo.TIF', dir + '/' + scene + '/' + scene + '_B2.TIF'])
-    subprocess.call(['gdal_translate', '-outsize', '15%', '15%', dir + '/B3-geo.TIF', dir + '/' + scene + '/' + scene + '_B3.TIF'])
-    subprocess.call(['gdal_translate', '-outsize', '15%', '15%', dir + '/B5-geo.TIF', dir + '/' + scene + '/' + scene + '_B5.TIF'])
+    # subprocess.call(['mkdir', direc + '/ready'])
+    subprocess.call(['gdal_translate', '-outsize', '15%', '15%', direc + '/B' + b1 + '-geo.TIF', direc_scene_scene + '_B' + b1 + '.TIF'])
+    subprocess.call(['gdal_translate', '-outsize', '15%', '15%', direc + '/B' + b2 + '-geo.TIF', direc_scene_scene + '_B' + b2 + '.TIF'])
+    subprocess.call(['gdal_translate', '-outsize', '15%', '15%', direc + '/B' + b3 + '-geo.TIF', direc_scene_scene + '_B' + b3 + '.TIF'])
+    print 'done resizing 3 images'
 
     # Call landsat-util
-    # import pdb; pdb.set_trace()
-    t = dir + '/' + scene
-    processor = Process(t, [5,3,2], dir, verbose=True)
+    t = direc + '/' + scene
+    processor = Process(t, [b1, b2, b3], direc, verbose=True)
     processor.run(pansharpen=False)
 
     # Convert black to transparent and save as PNG
-    subprocess.call(['convert', '-transparent', 'black', dir + '/' + scene + '/' + scene + '_bands_532.TIF', dir + '/final.png'])
+    file_in = '{}_bands_{}{}{}.TIF'.format(direc_scene_scene, b1, b2, b3)
+    subprocess.call(['convert', '-transparent', 'black', file_in, direc + '/final.png'])
 
     conne = boto.connect_s3(aws_access_key_id=AWS_ACCESS_KEY_ID,
          aws_secret_access_key=AWS_SECRET_ACCESS_KEY)
     b = conne.get_bucket('landsatproject')
     k = Key(b)
     k.key = scene + b1 + b2 + b3 + '.png'
-    k.set_contents_from_filename(dir + '/final.png')
-    k.get_contents_to_filename(dir + '/final.png')
+    k.set_contents_from_filename(direc + '/final.png')
+    k.get_contents_to_filename(direc + '/final.png')
     hello = b.get_key(scene + b1 + b2 + b3 + '.png')
+
+    # delete files
+    try:
+        rmtree(input_path)           # band images and composite
+    except OSError:
+        print input_path
+        print 'error deleting files'
+        pass
+
     # make public
     hello.set_canned_acl('public-read')
     out = hello.generate_url(0, query_auth=False, force_http=True)
