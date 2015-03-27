46359c7bce8871ad772d0c1fdedec7571f8cbf9b
Mark Saiget <saiget@gmail.com>
2015-03-27 02:57:20 -0700

    Use proper error handling. Only save preview image if render is successful. Reduce repetitive code.

--- app/app/views.py
+++ app/app/views.py
@@ -11,6 +11,7 @@ from pyramid.httpexceptions import HTTPFound
 from shutil import rmtree
 from pyramid.httpexceptions import exception_response
 from models import Rendered_Model
+import os.path
 
 
 AWS_ACCESS_KEY_ID = os.environ['AWS_ACCESS_KEY_ID']
@@ -20,101 +21,96 @@ AWS_SECRET_ACCESS_KEY = os.environ['AWS_SECRET_ACCESS_KEY']
 direc_world = '{}/world.tfw'.format(os.getcwd())
 
 
+def delete_directory(direc):
+    # delete files
+    print 'deleting directory: {}'.format(direc)
+    try:
+        rmtree(direc)
+    except OSError:
+        raise Exception('error deleting files')
+
+
 def process_image(direc, scene, root, path, row, b1, b2, b3):
     '''Method to process image'''
     direc_scene = '{direc}/{scene}'.format(direc=direc, scene=scene)
 
     direc_scene_scene = '{direc}/{sc}/{sc}'.format(direc=direc, sc=scene)
 
+    band_list = [b1, b2, b3]
+    o_list = ['o1', 'o2', 'o3']
     # Builds a string pointing towards the AWS Landsat datasets
-    o1 = '{root}{path}/{row}/{scene}/{scene}_B{band}.TIF.ovr'.format(
-                    root=root, path=path, row=row, scene=scene, band=b1)
-    o2 = '{root}{path}/{row}/{scene}/{scene}_B{band}.TIF.ovr'.format(
-                    root=root, path=path, row=row, scene=scene, band=b2)
-    o3 = '{root}{path}/{row}/{scene}/{scene}_B{band}.TIF.ovr'.format(
-                    root=root, path=path, row=row, scene=scene, band=b3)
+    for b, i in zip(band_list, o_list):
+        i = '{root}{path}/{row}/{scene}/{scene}_B{band}.TIF.ovr'.format(
+                    root=root, path=path, row=row, scene=scene, band=b)
 
     # Create a subdirectory
     if not os.path.exists(direc_scene):
         os.makedirs(direc_scene)
 
-    # Download previews from AWS
-    download(url=o1, path=direc_scene)
-    download(url=o2, path=direc_scene)
-    download(url=o3, path=direc_scene)
+    try:
+        # Download previews from AWS
+        for i in o_list:
+            download(url=i, path=direc_scene)
+    except:
+        out = 'https://raw.githubusercontent.com/recombinators/little-worker/master/failimages/faileddownload.png'
+        raise Exception('Download failed')
+
     print 'done downloading previews from aws'
 
-    # # rename files to remove .ovr
-    # os.rename('{direc}/{scene}_B{band}.TIF.ovr'.format(
-    #           direc=direc_scene, scene=scene, band=b1),
-    #           '{direc}/{scene}_B{band}.TIF'.format(
-    #           direc=direc_scene, scene=scene, band=b1))
-    # os.rename('{direc}/{scene}_B{band}.TIF.ovr'.format(
-    #           direc=direc_scene, scene=scene, band=b2),
-    #           '{direc}/{scene}_B{band}.TIF'.format(
-    #           direc=direc_scene, scene=scene, band=b2))
-    # os.rename('{direc}/{scene}_B{band}.TIF.ovr'.format(
-    #           direc=direc_scene, scene=scene, band=b3),
-    #           '{direc}/{scene}_B{band}.TIF'.format(
-    #           direc=direc_scene, scene=scene, band=b3))
-
     # Apply the stripped world file to the band previews.
-    subprocess.call(['geotifcp', '-e', direc_world,
-                     '{direc}/{scene}_B{band}.TIF.ovr'.format(
-                     direc=direc_scene, scene=scene, band=b1),
-                     direc_scene + '/B' + b1 + '-geo.TIF'])
-    subprocess.call(['geotifcp', '-e', direc_world,
-                     '{direc}/{scene}_B{band}.TIF.ovr'.format(
-                     direc=direc_scene, scene=scene, band=b2),
-                     direc_scene + '/B' + b2 + '-geo.TIF'])
-    subprocess.call(['geotifcp', '-e', direc_world,
-                     '{direc}/{scene}_B{band}.TIF.ovr'.format(
-                     direc=direc_scene, scene=scene, band=b3),
-                     direc_scene + '/B' + b3 + '-geo.TIF'])
+    for b in band_list:
+        subprocess.call(['geotifcp', '-e', direc_world,
+                         '{direc}/{scene}_B{band}.TIF.ovr'.format(
+                         direc=direc_scene, scene=scene, band=b),
+                         direc_scene + '/B' + b1 + '-geo.TIF'])
     print 'done applying world file to previews'
 
     # Resize each band
     # subprocess.call(['mkdir', direc + '/ready'])
-    subprocess.call(['gdal_translate', '-outsize', '15%', '15%',
-                     direc_scene + '/B' + b1 + '-geo.TIF',
-                     direc_scene_scene + '_B' + b1 + '.TIF'])
-    subprocess.call(['gdal_translate', '-outsize', '15%', '15%',
-                     direc_scene + '/B' + b2 + '-geo.TIF',
-                     direc_scene_scene + '_B' + b2 + '.TIF'])
-    subprocess.call(['gdal_translate', '-outsize', '15%', '15%',
-                     direc_scene + '/B' + b3 + '-geo.TIF',
-                     direc_scene_scene + '_B' + b3 + '.TIF'])
+    for b in band_list:
+        subprocess.call(['gdal_translate', '-outsize', '15%', '15%',
+                         direc_scene + '/B' + b + '-geo.TIF',
+                         direc_scene_scene + '_B' + b + '.TIF'])
     print 'done resizing 3 images'
 
     # Call landsat-util to merge images
     t = direc + '/' + scene
-    processor = Process(t, [b1, b2, b3], direc, verbose=True)
-    processor.run(pansharpen=False)
+    try:
+        processor = Process(t, [b1, b2, b3], direc, verbose=True)
+        processor.run(pansharpen=False)
+    except:
+        out = u'https://raw.githubusercontent.com/recombinators/little-worker/master/failimages/processfailed.png'
+        raise Exception('Processing/landsat-util failed')
 
     # Convert black to transparent and save as PNG
     file_in = '{}_bands_{}{}{}.TIF'.format(direc_scene_scene, b1, b2, b3)
     subprocess.call(['convert', '-transparent', 'black',
                     file_in, direc_scene + '/final.png'])
 
+    # check if final.png exists
+    if not os.path.isfile('{}/final.png'.format(direc_scene)):
+        out = u'https://raw.githubusercontent.com/recombinators/little-worker/master/failimages/finalpngnotcomposed.png'
+        raise Exception('Final.png not rendered')
+
     # upload to s3
-    conne = boto.connect_s3(aws_access_key_id=AWS_ACCESS_KEY_ID,
-                            aws_secret_access_key=AWS_SECRET_ACCESS_KEY)
-    b = conne.get_bucket('landsatproject')
-    k = Key(b)
-    k.key = scene + b1 + b2 + b3 + '.png'
-    k.set_contents_from_filename(direc_scene + '/final.png')
-    k.get_contents_to_filename(direc_scene + '/final.png')
-    hello = b.get_key(scene + b1 + b2 + b3 + '.png')
-    # make public
-    hello.set_canned_acl('public-read')
-    out = hello.generate_url(0, query_auth=False, force_http=True)
+    try:
+        conne = boto.connect_s3(aws_access_key_id=AWS_ACCESS_KEY_ID,
+                                aws_secret_access_key=AWS_SECRET_ACCESS_KEY)
+        b = conne.get_bucket('landsatproject')
+        k = Key(b)
+        k.key = scene + b1 + b2 + b3 + '.png'
+        k.set_contents_from_filename(direc_scene + '/final.png')
+        k.get_contents_to_filename(direc_scene + '/final.png')
+        hello = b.get_key(scene + b1 + b2 + b3 + '.png')
+        # make public
+        hello.set_canned_acl('public-read')
+        out = hello.generate_url(0, query_auth=False, force_http=True)
+    except:
+        out = u'https://raw.githubusercontent.com/recombinators/little-worker/master/failimages/connectiontoS3failed.png'
+        raise Exception('S3 upload failed')
 
     # delete files
-    print 'deleting directory: {}'.format(direc)
-    try:
-        rmtree(direc)           # band images and composite
-    except OSError:
-        print 'error deleting files'
+    delete_directory(direc)
 
     return out
 
@@ -132,23 +128,18 @@ def my_view(request):
     b3 = request.matchdict['b3']
 
     # Check if image already exists.
-    # out = Rendered_Model.preview_available(scene, b1, b2, b3)
-    # if out:
-    #     return HTTPFound(location=out)
+    out = Rendered_Model.preview_available(scene, b1, b2, b3)
+    if out:
+        return HTTPFound(location=out)
 
     try:
         out = process_image(direc, scene, root, path, row, b1, b2, b3)
-    except:
+        # store url in db
+        Rendered_Model.update_p_url(scene, b1, b2, b3, out)
+    except Exception as e:
+        print e
         # delete files
-        print 'deleting directory: {}'.format(direc)
-        try:
-            rmtree(direc)           # band images and composite
-        except OSError:
-            print 'error deleting files'
-        print 'ERROR rendering files'
-        out = u'https://raw.githubusercontent.com/jacquestardie/gifs/master/baby.gif'
-
-    # store url in db
-    Rendered_Model.update_p_url(scene, b1, b2, b3, out)
+        delete_directory(direc)
+        # out = u'https://raw.githubusercontent.com/jacquestardie/gifs/master/baby.gif'
 
     return HTTPFound(location=out)
