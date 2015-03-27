506e468133cf59a1adc04c93ff9f2fad54159009
Mark Saiget <saiget@gmail.com>
2015-03-26 20:49:52 -0700

    Change view to use existing geodata to render all preview images. This allows us to not download a 50+MB file for each preview and render more previews (that might have corrupt geo data). ** Geo data on all preview images hence forth is not correct!

--- app/app/views.py
+++ app/app/views.py
@@ -14,20 +14,20 @@ from pyramid.httpexceptions import exception_response
 from models import Rendered_Model
 
 
-direc = os.getcwd()
-
 AWS_ACCESS_KEY_ID = os.environ['AWS_ACCESS_KEY_ID']
 AWS_SECRET_ACCESS_KEY = os.environ['AWS_SECRET_ACCESS_KEY']
 
+# path of world(geodata) file
+direc_world = '{}/world.tfw'.format(os.getcwd())
 
-def process_image(direc, scene, root, path, row, b1, b2, b3, geo):
+
+def process_image(direc, scene, root, path, row, b1, b2, b3):
     '''Method to process image'''
     direc_scene = '{direc}/{scene}'.format(direc=direc, scene=scene)
 
     direc_scene_scene = '{direc}/{scene}/{scene}'.format(direc=direc, scene=scene)
 
     # Builds a string pointing towards the AWS Landsat datasets
-    # IE: http://landsat-pds.s3.amazonaws.com/L8/139/045/LC81390452014295LGN00/LC81390452014295LGN00_B3.TIF.ovr
     o1 = '{root}{path}/{row}/{scene}/{scene}_B{band}.TIF.ovr'.format(
                     root=root, path=path, row=row, scene=scene, band=b1)
     o2 = '{root}{path}/{row}/{scene}/{scene}_B{band}.TIF.ovr'.format(
@@ -35,23 +35,19 @@ def process_image(direc, scene, root, path, row, b1, b2, b3, geo):
     o3 = '{root}{path}/{row}/{scene}/{scene}_B{band}.TIF.ovr'.format(
                     root=root, path=path, row=row, scene=scene, band=b3)
 
-    # o1 = root + path + '/' + row + '/' + scene + '/' + scene + '_B' + b1 + '.TIF.ovr'
-    # o2 = root + path + '/' + row + '/' + scene + '/' + scene + '_B' + b2 + '.TIF.ovr'
-    # o3 = root + path + '/' + row + '/' + scene + '/' + scene + '_B' + b3 + '.TIF.ovr'
-
     # Create a subdirectory
     if not os.path.exists(direc_scene):
         os.makedirs(direc_scene)
 
     # Download a sacrificial band(1) using Landsat-util
-    dl = Downloader(verbose=True, download_dir=direc)
-    dl.download([str(scene)], [geo])
+    # dl = Downloader(verbose=True, download_dir=direc)
+    # dl.download([str(scene)], [geo])
 
     # Strip the geospatial metadata from a legitimate Landsat band.
     # Create a world.tfw to reapply to previews.
     # geo variable represents the band we grab geo data from
-    subprocess.call(['listgeo', '-tfw', direc_scene_scene + '_B{}.TIF'.format(geo)])
-    os.rename(direc_scene_scene + '_B{}.tfw'.format(geo), direc_scene + '/' + 'world.tfw')
+    # subprocess.call(['listgeo', '-tfw', direc + '_B{}.TIF'.format(geo)])
+    # os.rename(direc_scene_scene + '_B{}.tfw'.format(geo), direc_scene + '/' + 'world.tfw')
 
     # Download previews from AWS
     download(url=o1, path=direc_scene)
@@ -59,7 +55,8 @@ def process_image(direc, scene, root, path, row, b1, b2, b3, geo):
     download(url=o3, path=direc_scene)
     print 'done downloading previews from aws'
 
-    direc_world = '{}/world.tfw'.format(direc_scene)
+    print 'direc_world: {}'.format(direc_world)
+    import pdb; pdb.set_trace()
 
     # Apply the stripped world file to the band previews.
     subprocess.call(['geotifcp', '-e', direc_world,
@@ -114,12 +111,14 @@ def process_image(direc, scene, root, path, row, b1, b2, b3, geo):
     # store url in db
     Rendered_Model.update_p_url(scene, b1, b2, b3, out)
 
-    # # delete files
-    # print 'deleting directory: {}'.format(direc)
-    # try:
-    #     rmtree(direc)           # band images and composite
-    # except OSError:
-    #     print 'error deleting files'
+    # delete files
+    print 'deleting directory: {}'.format(direc)
+    try:
+        rmtree(direc)           # band images and composite
+    except OSError:
+        print 'error deleting files'
+
+    return out
 
 
 @view_config(route_name='home', renderer='json')
@@ -138,14 +137,10 @@ def my_view(request):
     if out:
         return HTTPFound(location=out)
 
-    # Some bands do not have correct geo data, so try multiple bands on failure
-    possible_bands = [9, 4, 3, 2, 1, 5, 6, 7, 10, 11]
-    for x in possible_bands:
-        try:
-            process_image(direc, scene, root, path, row, b1, b2, b3, x)
-        except:
-            print 'error rendering files with geo band {}'.format(x)
-            if x == 11:
-                raise exception_response(500)
+    try:
+        out = process_image(direc, scene, root, path, row, b1, b2, b3)
+    except:
+        print 'ERROR rendering files'
+        raise exception_response(500)
 
     return HTTPFound(location=out)
